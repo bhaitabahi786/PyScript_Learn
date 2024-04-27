8659b56763b9584c40def0b030a3da7c08642895
bhaitabahi786 <bhaitabahi786@gmail.com>
2024-04-27 19:36:28 +0530

    final done UI remaining

--- Expense_Calculator/main.py
+++ Expense_Calculator/main.py
@@ -1,9 +1,13 @@
 
-from js import document, Object
+from js import document, Object, localStorage
 from pyscript import window, display
+import json
 
 
 def addExpense(event):
+    
+    ExpIdCheck = []
+    
     print("addExpense")
     dateId = document.getElementById('date')
     categoryId = document.getElementById('category')
@@ -15,14 +19,29 @@ def addExpense(event):
     name = nameId.value
     amount = amountId.value
 
-    if date and amount and category :
+    keyAdd = Object.keys(localStorage)
+    
+    keyAdd.sort()
+    for i in keyAdd:
+        id = i.split('-')[1]
+        ExpIdCheck.append(int(id))
+        
+    if len(ExpIdCheck) == 0:
+        ExpId = 0
+    else:
+        ExpId = max(ExpIdCheck) + 1
 
+    if date and amount and category :
         if name == '':
             name = category
-
-        expense = {'date': date, 'category': category, 'name': name, 'amount': amount}
-        expenses.append(expense)
+        # expense = f'{{"date": {date}, "category": {category}, "name": {name}, "amount": {amount}}}'
+        expense = f'{{"date": "{date}", "category": "{category}", "name": "{name}", "amount": "{amount}"}}'
+        localStorage.setItem(f'expense-{ExpId}', expense)
+        print("addExp local storage ",expense)
+        
         renderExpenses()
+        ExpId += 1
+
 
     dateId.value = ''
     categoryId.value = 'Food'
@@ -33,14 +52,14 @@ def addExpense(event):
 
 def deleteExpense(e):
     index = e.target.parentNode.parentNode.id.split('-')[1]
-    # print("deleteExpense index ",index)
-    # print("deleteExpense expenses ",expenses)
-    # print("deleteExpense  ",index)
-    expenses.pop(int(index))
+    indexLocal = e.target.parentNode.parentNode.id
+    
+    localStorage.removeItem(indexLocal)
+
+    print("deleteExp local storage " ,indexLocal)
+    # localStorage.clear()
     renderExpenses()
-    # if int(index) < len(expenses):
-    #     expenses.pop(index)
-    #     renderExpenses()
+    
 
 def renderExpenses():
     tableBody = document.getElementById('expenseTableBody')
@@ -48,35 +67,53 @@ def renderExpenses():
 
     totalAmount = 0
 
-    for i, expense in enumerate(expenses):
-        print("RENDER exp ",expense)
+    keys = Object.keys(localStorage)
+    print("RENDER keys ",keys)
+    print("RENDER keys ",len(keys))
+
+    # value to sort the keys
+    keys.sort()
+    print("RENDER keys after sort ",keys)
+
+    for i in keys:
+        id = i.split('-')[1]
+        expense = localStorage.getItem(f'expense-{id}')
+        print("RENDER local expense ", expense)
         print("RENDER i ",i)
-        row = document.createElement('tr')
-        row.id = f'expense-{i}'
-        print("RENDER row ",row.id)
 
-        dateCell = document.createElement('td')
-        dateCell.textContent = expense['date']
-        categoryCell = document.createElement('td')
-        categoryCell.textContent = expense['category']
-        nameCell = document.createElement('td')
-        nameCell.textContent = expense['name']
-        amountCell = document.createElement('td')
-        amountCell.textContent = expense['amount']
-        totalAmount += float(expense['amount'])
-        actionCell = document.createElement('td')
-        deleteButton = document.createElement('button')
-        deleteButton.textContent = 'Delete'
-        deleteButton.onclick = deleteExpense
-        # deleteButton.setAttribute('onclick', f'deleteExpense({i})')
-        actionCell.appendChild(deleteButton)
+        if expense != None:
+            
+            row = document.createElement('tr')
+            row.id = f'expense-{id}'
+            
+            expenseId = json.loads(expense)
 
-        row.appendChild(dateCell)
-        row.appendChild(categoryCell)
-        row.appendChild(nameCell)
-        row.appendChild(amountCell)
-        row.appendChild(actionCell)
-        tableBody.appendChild(row)
+            dateCell = document.createElement('td')
+            dateCell.textContent = expenseId['date']
+
+            categoryCell = document.createElement('td')
+            categoryCell.textContent = expenseId['category']
+            nameCell = document.createElement('td')
+            nameCell.textContent = expenseId['name']
+            amountCell = document.createElement('td')
+            amountCell.textContent = expenseId['amount']
+            totalAmount += float(expenseId['amount'])
+            actionCell = document.createElement('td')
+            deleteButton = document.createElement('button')
+            deleteButton.textContent = 'Delete'
+            deleteButton.className = 'btn btn-danger'
+            deleteButton.onclick = deleteExpense
+            actionCell.appendChild(deleteButton)
+
+            row.appendChild(dateCell)
+            row.appendChild(categoryCell)
+            row.appendChild(nameCell)
+            row.appendChild(amountCell)
+            row.appendChild(actionCell)
+            tableBody.appendChild(row)
+
+        else :
+            print(f"RENDER local expense is None for expense-{i}")
 
     totalAmountDiv = document.getElementById('totalAmount')
     totalAmountDiv.textContent = totalAmount
